0f9129f0024cb7006b36abc47ef26ec97f85479b
Scott <muddcohd@gmail.com>
2022-06-29 16:21:39 -0400

    first commit

---
+++ global.js
@@ -0,0 +1,7 @@
+//GLOBALS - NO WINDOW
+
+//__dirname - path to current directory
+//__filename - filename
+// require - function to use module (Common JS)
+//module - info about current module
+//process - info about env where the program is being executed
